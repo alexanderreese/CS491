da0aa6cc4d56f25ca2b0509488510b375f56fd8e
KyberCritter <88512946+KyberCritter@users.noreply.github.com>
2023-10-23 12:41:27 -0500

    setting up in progress

---
+++ Assignment1/decision_forest.ipynb.py
@@ -0,0 +1,11 @@
+"""
+CS 491 Project 1
+Team Members: Scott Ratchford, Ben Fioresi, and Alexander Reese
+"""
+
+import tensorflow as tf
+import tensorflow_decision_forests as tfdf
+import pandas as pd
+# import numpy as np
+# import matplotlib.pyplot as plt
+
---
+++ README.md
@@ -0,0 +1,4 @@
+# Instructions for Assignment 1
+1. Install pipenv, if it is not already installed.
+2. Navigate to the Assignment1 folder.
+3. (incomplete)
