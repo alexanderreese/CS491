7d65265a1edf937c8d204b9b372441b7cf9e9b54
KyberCritter <88512946+KyberCritter@users.noreply.github.com>
2023-10-23 12:52:43 -0500

    update for colab

--- Assignment1/decision_forest.ipynb.py
+++ Assignment1/decision_forest.ipynb.py
@@ -2,10 +2,23 @@
 CS 491 Project 1
 Team Members: Scott Ratchford, Ben Fioresi, and Alexander Reese
 """
+try:
+  import tensorflow as tf
+except ModuleNotFoundError:
+  !pip install tensorflow
+  import tensorflow as tf
 
-import tensorflow as tf
-import tensorflow_decision_forests as tfdf
-import pandas as pd
+try:
+  import tensorflow_decision_forests as tfdf
+except ModuleNotFoundError:
+  !pip install tensorflow_decision_forests
+  import tensorflow_decision_forests as tfdf
+
+try:
+  import pandas as pd
+except ModuleNotFoundError:
+  !pip install pandas
+  import pandas as pd
+
 # import numpy as np
 # import matplotlib.pyplot as plt
-
